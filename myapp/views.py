901b05569419a9dacd4d60439a525adbffb7dd42
MoryDalton <102357168+MoryDalton@users.noreply.github.com>
2022-03-26 00:42:29 +0430

    Delete myapp directory

--- myapp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-def home(request):
-    return HttpResponse('hello this is home page by github and pythonanywhere')
